9cecaee9582dbb61ac3da28f8d9efb9cc1a77f58
Skeuo <skeuoobrien@protonmail.com>
2025-02-17 21:32:15 -0600

    script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simpleubuntu
 Stole this from reddit lol
+
+RUN AS ROOT
+chmod +x ./simpleubuntu.sh
+./simpleubuntu.sh
+
+script does everything for you
---
+++ simpleubuntu.sh
@@ -0,0 +1,11 @@
+snap remove lxd
+snap remove core20
+snap remove snapd
+apt autoremove --purge snapd
+apt-mark hold snapd
+rm -fr ~/snap
+rm -rf /var/cache/snapd
+rm -rf /snap
+rm -rf /var/snap
+rm -rf /var/lib/snapd
+pro config set apt_news=false
